4025c870abdfb14200dca3907e96a37f89722093
Pravesh Shukla <spravesh5@gmail.com>
2019-05-01 19:38:55 +0200

    add artist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtistListComponent } from './artist/artist-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/artist/artist-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-artist-list',
+    templateUrl:'./artist-list.component.html'
+})
+export class ArtistListComponent{
+
+}
