cb14bbbc97d9e71833c83ef97356e68c5d0cb65e
Pravesh Shukla <spravesh5@gmail.com>
2019-05-02 00:36:02 +0200

    work with list component

--- src/app/artist/artist-list.component.ts
+++ src/app/artist/artist-list.component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
+import { IArtist } from './artist';
 
 @Component({
     selector: 'app-artist-list',
-    templateUrl:'./artist-list.component.html'
+    templateUrl:'./artist-list.component.html',
+    styleUrls:['./artist-list.component.css']
 })
 export class ArtistListComponent{
+    pageTitle   : string    ="Artists list";
+    imageWidth  : number    = 50;
+    imageMargin : number    = 2;
+    showImage   : boolean   = false;
+    listFilter  : string    ="";
 
+    artists : IArtist[] = [
+        {
+            "Id":1,
+            "Name": "Chris Martin",
+            "City": "New York",
+            "Age": 30,
+            "Profession": "Singer",
+            "Available":"Yes",
+            "imageUrl":"./sample.jpg"
+        },
+        {
+            "Id":2,
+            "Name": "Adele",
+            "City": "London",
+            "Age": 35,
+            "Profession": "Singer",
+            "Available":"Yes",
+            "imageUrl":"./sample.jpg"
+        }          
+    ];
+
+    toggleImage():void{
+        this.showImage = !this.showImage;
+    }
 }
---
+++ src/app/artist/artist.ts
@@ -0,0 +1,9 @@
+export interface IArtist{
+    Id:number;
+    Name:string;    
+    Age:number;
+    City:string;
+    Profession:string;
+    Available:string;
+    imageUrl:string;    
+}
